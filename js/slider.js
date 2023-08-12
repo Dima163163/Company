4798fac18c4baa78a7163d45f90b18e3d618ea3d
Dima163163 <khlopovdima@gmail.com>
2023-08-12 22:07:26 +0400

    adaptiv

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		nextArrow: '<div class="sprite-next"></div>',
+		prevArrow: '<div class="sprite-prev"></div>'
+	});
+});
