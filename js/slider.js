6bc47acade5912f6f0a9b05a5d62ce9c93804a63
Dima163163 <khlopovdima@gmail.com>
2023-08-13 11:45:07 +0400

    fix slider

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
   $('.slider').slick({
-		slidesToShow: 1,
+		slidesToShow: 4,
 		slidesToScroll: 1,
 		nextArrow: '<div class="sprite-next"></div>',
-		prevArrow: '<div class="sprite-prev"></div>'
+		prevArrow: '<div class="sprite-prev"></div>',
+		responsive: [
+    {
+			breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+      breakpoint: 480,
+      settings: {
+				centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+				centerPaging: 80,
+      }
+	}]
 	});
 });
